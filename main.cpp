1a3827bba9b9afdd4f84e95af979234ca15598a3
svnnikita <svnnikitos@gmail.com>
2026-05-22 16:03:24 +0300

    feat: Изменить размер шрифта
     - Шрифт приложения увеличен до размера 14

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QFont appFont("Monospace", 14);
+    QApplication::setFont(appFont);
+
     MainWindow w;
     w.show();
     return a.exec();
